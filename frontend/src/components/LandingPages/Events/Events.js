c2d44923a98a4655817cb40f7bc09eaa61693ddf
ApoorvDubey23 <132251091+ApoorvDubey23@users.noreply.github.com>
2023-09-20 19:18:18 +0530

    [UPDATED]Matches (#140)

--- frontend/src/components/LandingPages/Events/Events.js
+++ frontend/src/components/LandingPages/Events/Events.js
@@ -1,26 +1,45 @@
 import React, { useState } from 'react';
 // import aquatics from './aquatics.png';
-import athletics from './athletics.png';
-import basketball from './basketball.png';
-import badminton from './badminton.png';
-import boxing from './boxing.png';
+import athletics from './image/athletics.png';
+import athleticshover from './image/athleticshover.png';
+import basketball from './image/basketball.png';
+import basketballhover from './image/basketballhover.png';
+import badminton from './image/badminton.png';
+import badmintonhover from './image/badmintonhover.png';
+import boxing from './image/boxing.png';
+import boxinghover from './image/boxinghover.png';
 // import carrom from './carrom.png'
-import chess from './chess.png';
-import cricket from './cricket.png';
-import football from './football.png';
-import handball from './handball.png';
+import chess from './image/chess.png';
+import chesshover from './image/chesshover.png';
+import cricket from './image/cricket.png';
+import crickethover from './image/crickethover.png';
+import football from './image/football.png';
+import footballhover from './image/footballhover.png';
+import handball from './image/handball.png';
+import handballhover from './image/handballhover.png';
 import question from './question.png';
-import hockey from './hockey.png';
-import kabaddi from './kabaddi.png';
-import khokho from './kho-kho.png';
-import cycling from './cycling.png';
-import squash from './squash.png';
-import powerlifting from './powerlifting.png';
-import tabletennis from './table-tennis.png';
-import taekwondo from './taekwondo.png';
-import tennis from './tennis.png';
-import volleyball from './volleyball.png';
-import weightlifting from './weightlifting.png';
+import hockey from './image/hockey.png';
+import hockeyhover from './image/hockeyhover.png';
+import kabaddi from './image/kabaddi.png';
+import kabaddihover from './image/kabaddihover.png';
+import khokho from './image/kho-kho.png';
+import khokhohover from './image/kho-khohover.png';
+import cycling from './image/cycling.png';
+import cyclinghover from './image/cyclinghover.png';
+import squash from './image/squash.png';
+import squashhover from './image/squashhover.png';
+import powerlifting from './image/powerlifting.png';
+import powerliftinghover from './image/powerliftinghover.png';
+import tabletennis from './image/table-tennis.png';
+import tabletennishover from './image/tabletennishover.png';
+import taekwondo from './image/taekwondo.png';
+import taekwondohover from './image/taekwondohover.png';
+import tennis from './image/tennis.png';
+import tennishover from './image/tennishover.png';
+import volleyball from './image/volleyball.png';
+import volleyballhover from './image/volleyballhover.png';
+import weightlifting from './image/weightlifting.png';
+import weightliftinghover from './image/weightliftinghover.png';
 import styles from './Events.module.css';
 import { Link } from 'react-router-dom';
 import Carousel from '../Home/Carousel/Carouselhp';
@@ -68,6 +87,140 @@ const Events = () => {
   const [showResults16, setShowResults16] = useState(false);
   const [showResults17, setShowResults17] = useState(false);
   const [showResults18, setShowResults18] = useState(false);
+const[athletics1,setathletics1]=useState(athletics);
+const athleticsMouseEnter = () => {
+  setathletics1(athleticshover);
+};
+const athleticsMouseLeave = () => {
+  setathletics1(athletics);
+};
+const[basketball1,setbasketball1]=useState(basketball);
+const basketballMouseEnter = () => {
+  setbasketball1(basketballhover);
+};
+const basketballMouseLeave = () => {
+  setbasketball1(basketball);
+};
+const[badminton1,setbadminton1]=useState(badminton);
+const badmintonMouseEnter = () => {
+  setbadminton1(badmintonhover);
+};
+const badmintonMouseLeave = () => {
+  setbadminton1(badminton);
+};
+const[boxing1,setboxing1]=useState(boxing);
+const boxingMouseEnter = () => {
+  setboxing1(boxinghover);
+};
+const boxingMouseLeave = () => {
+  setboxing1(boxing);
+};
+const[cricket1,setcricket1]=useState(cricket);
+const cricketMouseEnter = () => {
+  setcricket1(crickethover);
+};
+const cricketMouseLeave = () => {
+  setcricket1(cricket);
+};
+const[football1,setfootball1]=useState(football);
+const footballMouseEnter = () => {
+  setfootball1(footballhover);
+};
+const footballMouseLeave = () => {
+  setfootball1(football);
+};
+const[handball1,sethandball1]=useState(handball);
+const handballMouseEnter = () => {
+  sethandball1(handballhover);
+};
+const handballMouseLeave = () => {
+  sethandball1(handball);
+};
+const[hockey1,sethockey1]=useState(hockey);
+const hockeyMouseEnter = () => {
+  sethockey1(hockeyhover);
+};
+const hockeyMouseLeave = () => {
+  sethockey1(hockey);
+};
+const[kabaddi1,setkabaddi1]=useState(kabaddi);
+const kabaddiMouseEnter = () => {
+  setkabaddi1(kabaddihover);
+};
+const kabaddiMouseLeave = () => {
+  setkabaddi1(kabaddi);
+};
+const[khokho1,setkhokho1]=useState(khokho);
+const khokhoMouseEnter = () => {
+  setkhokho1(khokhohover);
+};
+const khokhoMouseLeave = () => {
+  setkhokho1(khokho);
+};
+const[cycling1,setcycling1]=useState(cycling);
+const cyclingMouseEnter = () => {
+  setcycling1(cyclinghover);
+};
+const cyclingMouseLeave = () => {
+  setcycling1(cycling);
+};
+const[squash1,setsquash1]=useState(squash);
+const squashMouseEnter = () => {
+  setsquash1(squashhover);
+};
+const squashMouseLeave = () => {
+  setsquash1(squash);
+};
+const[tabletennis1,settabletennis1]=useState(tabletennis);
+const tabletennisMouseEnter = () => {
+  settabletennis1(tabletennishover);
+};
+const tabletennisMouseLeave = () => {
+  settabletennis1(tabletennis);
+};
+const[powerlifting1,setpowerlifting1]=useState(powerlifting);
+const powerliftingMouseEnter = () => {
+  setpowerlifting1(powerliftinghover);
+};
+const powerliftingMouseLeave = () => {
+  setpowerlifting1(powerlifting);
+};
+const[weightlifting1,setweightlifting1]=useState(weightlifting);
+const weightliftingMouseEnter = () => {
+  setweightlifting1(weightliftinghover);
+};
+const weightliftingMouseLeave = () => {
+  setweightlifting1(weightlifting);
+};
+const[taekwondo1,settaekwondo1]=useState(taekwondo);
+const taekwondoMouseEnter = () => {
+  settaekwondo1(taekwondohover);
+};
+const taekwondoMouseLeave = () => {
+  settaekwondo1(taekwondo);
+};
+const[tennis1,settennis1]=useState(tennis);
+const tennisMouseEnter = () => {
+  settennis1(tennishover);
+};
+const tennisMouseLeave = () => {
+  settennis1(tennis);
+};
+const[volleyball1,setvolleyball1]=useState(volleyball);
+const volleyballMouseEnter = () => {
+  setvolleyball1(volleyballhover);
+};
+const volleyballMouseLeave = () => {
+  setvolleyball1(volleyball);
+};
+const[chess1,setchess1]=useState(volleyball);
+const chessMouseEnter = () => {
+  setchess1(chesshover);
+};
+const chessMouseLeave = () => {
+  setchess1(chess);
+};
+
 
   const onClick0 = () => {
     setShowResults0(!showResults0);
@@ -481,18 +634,20 @@ const Events = () => {
           <div
             className={`${styles.col_md_7} ${styles.heading_section} ${styles.text_center}`}
           >
-            <h3
+            {/* <h3
               className={`${styles.subheading}`}
               style={{ color: '#6db549', fontWeight: 'bold' }}
             >
               Find Your Interest
-            </h3>
-            <h2 className={`${styles.mb_1} ${styles.H2}`} style={{ color: 'white' }}>
-            Sports &amp; Games
-            </h2>
+            </h3> */}
+           
           </div>
         </div>
-
+      <div className={styles.maindiv}>
+      <h2 className={`${styles.mb_1} ${styles.H2}`} >
+            Sports &amp; Games
+            </h2>
+            <div className={styles.scrollablediv}>
         <div className={`${styles.row}`}>
           {/* <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
@@ -508,48 +663,61 @@ const Events = () => {
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
-            id="0"
+            id="0"      onMouseEnter={athleticsMouseEnter}
+            onMouseLeave={athleticsMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={athletics}
+              src={athletics1}
               alt=""
               onClick={onClick0}
             />
+            <p>Athletics</p>
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="1"
+            onMouseEnter={badmintonMouseEnter}
+            onMouseLeave={badmintonMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={badminton}
+              src={badminton1}
               alt=""
               onClick={onClick1}
             />
+             <p>Badminton</p>
+
+                
           </div>
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="2"
+            onMouseEnter={basketballMouseEnter}
+            onMouseLeave={basketballMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={basketball}
+              src={basketball1}
               alt=""
               onClick={onClick2}
             />
+          <p>Basketball</p>
           </div>
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="3"
+            onMouseEnter={boxingMouseEnter}
+            onMouseLeave={boxingMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={boxing}
+              src={boxing1}
               alt=""
               onClick={onClick3}
             />
+                 <p>Boxing</p>
           </div>
           {/*<div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
@@ -596,47 +764,61 @@ const Events = () => {
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="0"
+            onMouseEnter={chessMouseEnter}
+            onMouseLeave={chessMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={chess}
+              src={chess1}
               alt=""
               onClick={onClick4}
             />
+             <p>Chess</p>
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="1"
+            onMouseEnter={cricketMouseEnter}
+            onMouseLeave={cricketMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={cricket}
+              src={cricket1}
               alt=""
               onClick={onClick5}
             />
+             <p>Cricket</p>
           </div>
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="2"
+            onMouseEnter={cyclingMouseEnter}
+            onMouseLeave={cyclingMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={cycling}
+              src={cycling1}
               alt=""
               onClick={onClick6}
             />
+             <p>Cycling</p>
+
           </div>
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="3"
+            onMouseEnter={footballMouseEnter}
+            onMouseLeave={footballMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={football}
+              src={football1}
               alt=""
               onClick={onClick7}
             />
+             <p>Football</p>
+
           </div>
 
           {/*<div
@@ -674,48 +856,64 @@ const Events = () => {
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="0"
+            onMouseEnter={handballMouseEnter}
+            onMouseLeave={handballMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={handball}
+              src={handball1}
               alt=""
               onClick={onClick8}
             />
+             <p>Handball</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="1"
+            onMouseEnter={hockeyMouseEnter}
+            onMouseLeave={hockeyMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={hockey}
+              src={hockey1}
               alt=""
               onClick={onClick9}
             />
+             <p>Hockey</p>
+
           </div>
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="2"
+            onMouseEnter={kabaddiMouseEnter}
+            onMouseLeave={kabaddiMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={kabaddi}
+              src={kabaddi1}
               alt=""
               onClick={onClick10}
             />
+             <p>Kabaddi</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="3"
+            onMouseEnter={khokhoMouseEnter}
+            onMouseLeave={khokhoMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={khokho}
+              src={khokho1}
               alt=""
               onClick={onClick11}
             />
+             <p>kho-kho</p>
+
           </div>
           {/*<div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
@@ -761,49 +959,65 @@ const Events = () => {
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="0"
+            onMouseEnter={powerliftingMouseEnter}
+            onMouseLeave={powerliftingMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={powerlifting}
+              src={powerlifting1}
               alt=""
               onClick={onClick12}
             />
+             <p>Powerlifting</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="1"
+            onMouseEnter={squashMouseEnter}
+            onMouseLeave={squashMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={squash}
+              src={squash1}
               alt=""
               onClick={onClick13}
             />
+             <p>Squash</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="2"
+            onMouseEnter={tabletennisMouseEnter}
+            onMouseLeave={tabletennisMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={tabletennis}
+              src={tabletennis1}
               alt=""
               onClick={onClick14}
             />
+             <p>TableTennis</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="3"
+            onMouseEnter={taekwondoMouseEnter}
+            onMouseLeave={taekwondoMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={taekwondo}
+              src={taekwondo1}
               alt=""
               onClick={onClick15}
             />
+             <p>Taekwondo</p>
+
           </div>
           {/* {showResults12 ? (
             <Kabbadi changeState={(showResult12) => setShowResults12(false)} />
@@ -840,37 +1054,49 @@ const Events = () => {
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="0"
+            onMouseEnter={tennisMouseEnter}
+            onMouseLeave={tennisMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={tennis}
+              src={tennis1}
               alt=""
               onClick={onClick16}
             />
+             <p>Tennis</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="1"
+            onMouseEnter={volleyballMouseEnter}
+            onMouseLeave={volleyballMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={volleyball}
+              src={volleyball1}
               alt=""
               onClick={onClick17}
             />
+             <p>Volleyball</p>
+
           </div>
 
           <div
             className={`${styles.col_2} ${styles.padding} ${styles.shake}`}
             id="2"
+            onMouseEnter={weightliftingMouseEnter}
+            onMouseLeave={weightliftingMouseLeave}
           >
             <img
               className={`${styles.img_responsive} ${styles.hvr_buzz_out} ${styles.grayscale}`}
-              src={weightlifting}
+              src={weightlifting1}
               alt=""
               onClick={onClick18}
             />
+             <p>Weightlifting</p>
+
           </div>
 
           {/* <div
@@ -896,7 +1122,8 @@ const Events = () => {
           ) : null}
         </div>
       </div>
-
+      </div>
+      </div>
       <div className={`${styles.container}`}>
         <div className={`${styles.row} ${styles.mt_5} ${styles.pt_5}`}>
           {/* <div className={`${styles.col_md_3}`}></div> */}
