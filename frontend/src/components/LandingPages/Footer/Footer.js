55c8b0493c62e53a554120f6095d3fcc883df070
vipul <vipulkumar2426@gmail.com>
2023-09-28 20:42:03 +0530

    updated-mail links

--- frontend/src/components/LandingPages/Footer/Footer.js
+++ frontend/src/components/LandingPages/Footer/Footer.js
@@ -21,26 +21,26 @@ const Footer = () => {
                   <div>
                     <h5><b>Convenor:</b></h5>
                     <h6 className={styles.name}>Himanshu Ranjan</h6>
-                    <h6>himanshu.ranjan.met20@itbhu.ac.in</h6>
+                    <h6>himanshu.ranjan.met20@iitbhu.ac.in</h6>
                   </div>
                   <div>
                     <h5><b>Co-convenors:</b></h5>
                     <h6 className={styles.name}>Harsh Lalit Parmar</h6>
-                    <h6>harsh.lalitparmar.mat20@itbhu.ac.in</h6>
+                    <h6>harsh.lalitparmar.mat20@iitbhu.ac.in</h6>
                     <h6 className={styles.name}>Aditya Ayush Mishra</h6>
-                    <h6>aditya.ayushmishra.met20@itbhu.ac.in</h6>
+                    <h6>aditya.ayushmishra.met20@iitbhu.ac.in</h6>
                   </div>
                 </div>
                 <div className={styles.publicity}>
                   <h5><b>Publicity:</b></h5>
                   <h6 className={styles.name}>Nimish Parmar</h6>
-                  <h6>nimish.parmar.civ21@itbhu.ac.in</h6>
+                  <h6>nimish.parmar.civ21@iitbhu.ac.in</h6>
                   <h6>+91 74006 94759</h6>
                   <h6 className={styles.name}>Adarsh Raj</h6>
-                  <h6>adarsh.raj.civ21@itbhu.ac.in </h6>
+                  <h6>adarsh.raj.civ21@iitbhu.ac.in </h6>
                   <h6>+91 88628 66770</h6>
                   <h6 className={styles.name}>Sarthak Gupta</h6>
-                  <h6>sarthak.gupta.civ21@itbhu.ac.in</h6>
+                  <h6>sarthak.gupta.civ21@iitbhu.ac.in</h6>
                   <h6>+91 63920 03370</h6>
                 </div>
               </div>
@@ -48,22 +48,22 @@ const Footer = () => {
                 <div className={styles.events}>
                   <h5><b>Events:</b></h5>
                   <h6 className={styles.name}>Gurmeet Punia</h6>
-                  <h6>gurmeet.student.civ21@itbhu.ac.in</h6>
+                  <h6>gurmeet.student.civ21@iitbhu.ac.in</h6>
                   <h6>+91 97858 15941</h6>
                   <h6 className={styles.name}>Dhruv Jain</h6>
-                  <h6>dhruv.jain.cd.mec21@itbhu.ac.in</h6>
+                  <h6>dhruv.jain.cd.mec21@iitbhu.ac.in</h6>
                   <h6>+91 95489 62930</h6>
                 </div>
                 <div className={styles.hospitality}>
                   <h5><b>Hospitality:</b></h5>
                   <h6 className={styles.name}>Aryan Singh</h6>
-                  <h6>aryan.singh.met21@itbhu.ac.in</h6>
+                  <h6>aryan.singh.met21@iitbhu.ac.in</h6>
                   <h6>+91 99972 55604</h6>
                   <h6 className={styles.name}>Gaurav Maharshi</h6>
-                  <h6>gaurav.maharshi.cer21@itbhu.ac.in </h6>
+                  <h6>gaurav.maharshi.cer21@iitbhu.ac.in </h6>
                   <h6>+91 87624 49914</h6>
                   <h6 className={styles.name}>Deepanshu</h6>
-                  <h6>deepanshu.student.cer21@itbhu.ac.in</h6>
+                  <h6>deepanshu.student.cer21@iitbhu.ac.in</h6>
                   <h6>+91 91168 98755</h6>
                 </div>
               </div>
@@ -71,7 +71,7 @@ const Footer = () => {
                 <div className={styles.links}>
                   <div className={styles.logo}>
                     <i class="fa-solid fa-envelope fa-2xl"></i>
-                    <a href="spardha@itbhu.ac.in" className={styles.link}>spardha@itbhu.ac.in</a>
+                    <a href="mailto:spardha@iitbhu.ac.in" className={styles.link}>spardha@iitbhu.ac.in</a>
                   </div>
                   <div className={styles.logo} style={{
                     display: "flex",
@@ -90,7 +90,7 @@ const Footer = () => {
                   </div>
                   <div className={`${styles.logo}`}>
                   <h5>For Tech Related Query</h5>
-                  <a href="tech@spardha.org.in" className={styles.link}>tech@spardha.org.in</a>
+                  <a href="mailto:tech@spardha.org.in" className={styles.link}>tech@spardha.org.in</a>
                   </div>
                   <br />
                 </div>
