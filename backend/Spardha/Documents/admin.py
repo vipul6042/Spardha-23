76447ce2eb604ee7b07e0bf485475aa5dd897870
Mradul Singh <mradulsingh524.mss@gmail.com>
2023-09-29 11:47:31 +0530

    [UPDATED] ADMIN folder (#205)

--- backend/Spardha/Documents/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Document
-
-# Register your models here.
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ['user_id', 'document', 'is_verified', 'verified_by', 'verification_time']
-
-admin.site.register(Document, DocumentAdmin)
--- backend/Spardha/Manager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import show_home, all_export, game_export, user_export
-
-urlpatterns = [
-    path("", show_home, name="index"),
-    path("all", all_export, name="index"),
-    path("game/<id>/", game_export, name="index"),
-    path("user/<id>/<player>/", user_export, name="index"),
-]
